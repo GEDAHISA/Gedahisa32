8956ffdc1f4f2c7f368d892efb5a7b33db2ac739
GEDAHISA <106118710+GEDAHISA@users.noreply.github.com>
2023-01-30 21:45:46 -0300

    gedahisa32

---
+++ script.js
@@ -0,0 +1,18 @@
+// const mudar = document.getElementById("mudar")
+
+// const agora = nee Date ();
+
+// mudar.innerText = "Bom dia"
+
+
+function ligarDesligar(){
+
+
+const luz = document.getElementById("luz");
+luz.classList.toggle("ligada")
+
+}
+
+
+
+
